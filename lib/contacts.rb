19479c09f14b830d71114a2b5e509b7b6c6446e6
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-08-04 22:39:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,4 +18,5 @@ def remove_strawberry(contacts)
 freddy.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
       value.delete_if{ |ice_cream| ice_cream == "strawberry" }
+    end
 end
